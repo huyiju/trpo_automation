eacf1dc9125c82e7a42eeed7191f84c08d578033
Maxim <MaXLyuT2000@gmail.com>
2020-04-23 15:26:39 +0300

    Рефакторинг 2.0 с pattern

--- email/18-ISbo-2a/Validation.py
+++ email/18-ISbo-2a/Validation.py
@@ -1,22 +1,11 @@
 import log_method
+from pattern import *
 
 
 @log_method.log_method_info
 def validation(head_of_msg,body_of_msg):
-    
-    Subjects_list = ['ТРПО'] # Список предметов
-    SubjectNumber_list = ['ЛР№1', 'Лабораторная работа №1', 
-                          'ЛР№2', 'Лабораторная работа №2',
-                          'ЛР№3', 'Лабораторная работа №3',
-                          'ЛР№4', 'Лабораторная работа №4',
-                          'ЛР№5', 'Лабораторная работа №5',
-                          'ЛР№6', 'Лабораторная работа №6',
-                          'ЛР№7', 'Лабораторная работа №7',
-                          'ЛР№8', 'Лабораторная работа №8',
-                          'ЛР№9', 'Лабораторная работа №9']
     Errors_list = [] # Список ошибок
-    Greeting_list = ['Добрый день','Добрый вечер'] # Список приветствий
-    for x in Subjects_list: # Проверка на название предмета
+    for x in SUBJECT_LIST: # Проверка на название предмета
         a = head_of_msg.find(x)
         if a != -1:
             break
@@ -24,7 +13,7 @@ def validation(head_of_msg,body_of_msg):
         log_method.logger.warning('validation: The name of \
                                    item is incorrect.')
         Errors_list.append('неверно указано название предмета')
-    for x in SubjectNumber_list: # Проверка на номер лабораторной
+    for x in SUBJECTNUMBER_LIST: # Проверка на номер лабораторной
         a = head_of_msg.find(x)
         if a != -1:
             a = head_of_msg.find('№')
@@ -35,7 +24,7 @@ def validation(head_of_msg,body_of_msg):
                                    is incorrect.')
         Errors_list.append('неверно указан номер ЛР')
         Number = None
-    for x in Greeting_list: # Проверка на приветствие 
+    for x in GREATING_LIST: # Проверка на приветствие 
         a = body_of_msg.find(x)
         if a != -1:
             break
@@ -61,21 +50,18 @@ def url_cheack(Number,body_of_msg): # Проверка на наличие URL. 
         if Number == x:
             a = body_of_msg.find('http')
             counter = 0
-            # Ищем первый пробел после http
-            while  a + counter < len(body_of_msg)-1:
+            while  a + counter < len(body_of_msg)-1:# Ищем пробел после http
                 if body_of_msg[a + counter] != ' ':
                     counter = counter + 1
                 else:
-                    break
-             
+                    break        
             if a+counter == len(body_of_msg)-1: # Дошли не найдя пробелов
                 if body_of_msg[a + counter] == '.':
                     URL = body_of_msg[a:a + counter]
                     return(URL)
                 else:
                     URL=body_of_msg[a:a + counter + 1]
-                    return(URL)
-                   
+                    return(URL) 
             while a + counter >= a:
                 if body_of_msg[a + counter] == '.':
                     URL = body_of_msg[a:a + counter]
@@ -83,6 +69,3 @@ def url_cheack(Number,body_of_msg): # Проверка на наличие URL. 
                     return (URL)                  
                 if body_of_msg[a + counter] != '.':
                     counter = counter - 1  
-
-    
-
